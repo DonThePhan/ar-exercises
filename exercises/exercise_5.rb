008d7e53577416677fc15468ef7da216e98def25
Donny <donthephan@gmail.com>
2022-02-01 18:26:53 +0000

    exercise 6 complete

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+entire_company_revenue = Store.sum(:annual_revenue)
+average_annual_revenue = entire_company_revenue/Store.count
+
+puts "Entire Company Revenue: #{entire_company_revenue}"
+puts "Average Annual Revenue for all stores: #{average_annual_revenue}"
+
+stores_1M_or_up = Store.where(annual_revenue: 1000000..Float::INFINITY).count
+puts "#{stores_1M_or_up}"
