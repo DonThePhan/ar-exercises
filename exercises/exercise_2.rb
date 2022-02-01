83a6d8064191760b009c2ad7e378afd9f5aca2d2
Donny <donthephan@gmail.com>
2022-02-01 17:48:18 +0000

    exercises 2 and 3 complete

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -6,5 +6,7 @@ puts "----------"
 # Your code goes here ...
 
 @store1 = Store.find_by(id: 1)
-@store2 = Store.find_by(id: 1)
-puts @store1
+@store2 = Store.find_by(id: 2)
+
+@store1.name = 'Stanley'
+@store1.save
